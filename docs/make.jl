6005f2f33ec407c6065d35dc4e8d8c8d06e46e31
Toby Driscoll <driscoll@udel.edu>
2022-09-20 10:02:38 -0400

    Update docs config

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,9 @@
 using Documenter, HaltonSequences
 
-makedocs(sitename="HaltonSequences",
-    format = :html,
-    authors = "Toby Driscoll",
-    pages = [
-        "Home" => "index.md",
-        "Functions and types" => "funcs.md"
-        ],
-    doctest = true
-    )
+makedocs(sitename="HaltonSequences")
 
 deploydocs(
     repo = "github.com/tobydriscoll/HaltonSequences.jl.git",
-    julia = "0.7",
-    deps = nothing,
-    make = nothing
+    deploy_config = Documenter.GitHubActions(),
 )
     
